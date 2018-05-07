0285f9e00a4da34b03d6731d24590f08dd5da28b
Rok Zidarn <rz3817@student.uni-lj.si>
2018-05-07 19:35:45 +0200

    Different approach

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import os
-import numpy as np
-from functools import reduce
 from xml.etree import ElementTree as Et
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-from keras.models import Model
-from keras import layers
-from keras import Input
+
+def parse_file(directory, file):
+    root = Et.parse(os.path.join(directory, file)).getroot()
+    instances = []  # list of instances/stories
+    questions = []  # 1 question per instance
+    answers = []  # true answer per each question, ignore false
+
+    # transforming text data to arrays
+    for instance in root:  # instance/story
+        instances.append(instance[0].text)
+        for question in instance[1]:  # multiple questions
+            questions.append(question.attrib['text'])
+            if question[0].attrib['correct'] == 'True':  # 2 possible answers; true then false
+                answers.append(question[0].attrib['text'])
+            else:  # 2 possible answers; false then true
+                answers.append(question[1].attrib['text'])
+            break
+
+    return instances, questions, answers
+
+# MAIN
 
 # file locations
 data_dir = 'Data'
@@ -15,124 +29,16 @@ dev_file = 'dev-data.xml'
 test_file = 'test-data.xml'
 first_file = 'first-data.xml'
 
-# xml file parsing
-root = Et.parse(os.path.join(data_dir, first_file)).getroot()
-instances = []  # list of instances/stories
-questions = []
-answers = []
-
-num_instances = 0
-num_questions = 0
-num_answers = 0  # only true answers
-
-# transforming text data to arrays
-for instance in root:  # instance/story
-    num_instances += 1
-    instances.append(instance[0].text)
-    instance_questions = []
-    for question in instance[1]:  # multiple questions
-        num_questions += 1
-        instance_questions.append(question.attrib['text'])
-        if question[0].attrib['correct'] == 'True':  # 2 possible answers; true then false
-            num_answers += 1
-            answers.append(question[0].attrib['text'])
-        else:  # 2 possible answers; false then true
-            num_answers += 1
-            answers.append(question[1].attrib['text'])
-    questions.append(instance_questions)
+train_data = parse_file(data_dir, train_file)
 
 # vocabulary stats
-max_len_instance = len(str(max(instances, key=len)).split())
-max_len_question = len(str(max(questions, key=len)).split())
-max_len_answer = len(str(max(answers, key=len)).split())
-
-print('#data (I,Q,A):', num_instances, num_questions, num_answers)
-print('#max lengths (I,Q,A):', max_len_instance, max_len_question, max_len_answer)
-
 max_words = 10000  # 10k most common words
+max_len_instance = len(str(max(train_data[0], key=len)).split())
+max_len_question = len(str(max(train_data[1], key=len)).split())
+max_len_answer = len(str(max(train_data[2], key=len)).split())
 
-# tokenizing + word index
-tokenizer = Tokenizer(num_words=max_words)
+print('data (I,Q,A): ', len(train_data[0]), len(train_data[1]), len(train_data[2]))
+print('max lengths (I,Q,A):', max_len_instance, max_len_question, max_len_answer)
 
-# instances
-all_instances = ' '.join(instances)
-tokenizer.fit_on_texts(all_instances)  # words represented as a number
-word_index_instances = tokenizer.word_index  # dictionary of distinct words -> (key, value) - ('word', index) -> ('the', 152)
-sequences = tokenizer.texts_to_sequences(all_instances)  # each instance represented as numerical array
-instances_data = pad_sequences(sequences, maxlen=max_len_instance)  # pads sequences to the same length
-
-# questions
-qs = reduce(lambda x, y: x+y, questions)
-all_questions = ' '.join(qs)
-tokenizer.fit_on_texts(all_questions)
-word_index_questions = tokenizer.word_index
-sequences = tokenizer.texts_to_sequences(all_questions)
-questions_data = pad_sequences(sequences, maxlen=max_len_question)
-
-# answers
-all_answers = ' '.join(answers)
-tokenizer.fit_on_texts(all_answers)
-word_index_answers = tokenizer.word_index
-sequences = tokenizer.texts_to_sequences(all_answers)
-answers_data = pad_sequences(sequences, maxlen=max_len_answer)
-
-# print(word_index)
-print('#distinct words (I,Q,A):', len(word_index_instances), len(word_index_questions), len(word_index_answers))
-
-# glove embeddings
-glove_dir = '../Glove'
-embeddings_index = {}  # 400k pretrained word embeddings
-f = open(os.path.join(glove_dir, 'glove.6B.100d.txt'), encoding="utf8")
-# (word, vector representation of the word) -> ('the', [-0.038194 -0.24487   0.72812 ...])
-
-for line in f:
-    values = line.split()
-    word = values[0]
-    coefficients = np.asarray(values[1:], dtype='float32')
-    embeddings_index[word] = coefficients
-f.close()
-
-# creation of embedding matrices
-embedding_dim = 100
-
-#instances
-embedding_matrix_instances = np.zeros((max_words, embedding_dim))
-for word, i in word_index_instances.items():  # goes through all the words in the instances, each word represented by unique number
-    if i < max_words:
-        embedding_vector = embeddings_index.get(word)
-        if embedding_vector is not None:  # if the word is found in the pretrained embeddings, get vector for the word
-            embedding_matrix_instances[i] = embedding_vector
-        # words not found in the embedding index will be all zeros
-
-#questions
-embedding_matrix_questions = np.zeros((max_words, embedding_dim))
-for word, i in word_index_questions.items():  # goes through all the words in the instances, each word represented by unique number
-    if i < max_words:
-        embedding_vector = embeddings_index.get(word)
-        if embedding_vector is not None:  # if the word is found in the pretrained embeddings, get vector for the word
-            embedding_matrix_questions[i] = embedding_vector
-        # words not found in the embedding index will be all zeros
-
-#answers
-embedding_matrix_answers = np.zeros((max_words, embedding_dim))
-for word, i in word_index_answers.items():  # goes through all the words in the instances, each word represented by unique number
-    if i < max_words:
-        embedding_vector = embeddings_index.get(word)
-        if embedding_vector is not None:  # if the word is found in the pretrained embeddings, get vector for the word
-            embedding_matrix_answers[i] = embedding_vector
-        # words not found in the embedding index will be all zeros
-
-# model
-text_input = Input(shape=(None,), dtype='int32', name='instances_data')  # name attribute = variable called "text"
-embedded_text = layers.Embedding(64, len(word_index_instances))(text_input)
-encoded_text = layers.LSTM(32)(embedded_text)
-
-question_input = Input(shape=(None,), dtype='int32', name='questions_data')
-embedded_question = layers.Embedding(32, len(word_index_questions))(question_input)
-encoded_question = layers.LSTM(16)(embedded_question)
-
-concatenated = layers.concatenate([encoded_text, encoded_question], axis=-1)
-answer = layers.Dense(len(word_index_answers), activation='softmax')(concatenated)
-
-model = Model([text_input, question_input], answer)
-model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['acc'])
+# TODO: fix IQA lengths
+# TODO: build vocabulary + vectorize
